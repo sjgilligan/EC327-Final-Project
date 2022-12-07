d46f36ac174415df64bad497bf8488cb3f7163b5
Nick Ramondo <114350485+ramondon1@users.noreply.github.com>
2022-12-07 16:49:42 -0500

    Update Point2d.cpp

--- Point2d.cpp
+++ Point2d.cpp
@@ -11,27 +11,6 @@ using namespace std;
         x = in_x;
         y = in_y;
     }
-
-
-std::ostream& operator << (std::ostream& os, const Point2d p1){
-    os << "(" << p1.x << "," << p1.y << ")" << endl;
-    return os;
-}
-Point2d operator + (const Point2d& p1, const Vector2d& v1){
-    return Point2d(p1.x + v1.x, p1.y + v1.y);
-}
-Vector2d operator - (Point2d& p1, Point2d& p2){
-    return Vector2d(p1.x - p2.x, p1.y - p2.y);
-}
-
-
-
-double getDistanceBetween(Point2d p1, Point2d p2){
-    double newy = p2.y - p1.y;
-    double newx = p2.x - p1.x;
-    double res = sqrt(newx*newx + newy*newy);
-    return res;
-}
 Point2d::~Point2d(){
 	cout << "Point2d Destructed" << endl;
 }
